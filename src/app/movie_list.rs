77ac8c90a4d019c247cd4f02bcc21f67c768c61e
BitUnWise <evanscottfairley@gmail.com>
2025-10-27 11:43:52 -0700

    Improved sorting

--- src/app/movie_list.rs
+++ src/app/movie_list.rs
@@ -1,14 +1,16 @@
-use std::time::Duration;
+use std::{cmp::Ordering, time::Duration};
 
 use iddqd::IdHashMap;
 use leptos::prelude::*;
 use leptos_fetch::{QueryClient, QueryOptions, QueryScope};
-use thaw::{Tab, TabList};
+use thaw::Flex;
 
 use crate::{
-    app::get_movies,
+    app::{get_movies, movie_list::sort_button::SortButton},
     movies::{Movie, MovieCard},
 };
+
+mod sort_button;
 
 #[component]
 pub(crate) fn movie_list() -> impl IntoView {
@@ -17,27 +19,21 @@ pub(crate) fn movie_list() -> impl IntoView {
         .with_options(QueryOptions::new().with_refetch_interval(Duration::from_secs(360)))
         .with_title("Movies");
     let resource = client.resource(query, move || ());
-    let sort_key = RwSignal::new("title".to_string());
-    let sort_order = client.local_resource(
-        async | sort: String | { SortOrder::from_str(sort.as_str()) },
-        move || sort_key.get(),
-    );
+    let sort_order = RwSignal::new(SortOrder::default());
     view! {
+        <Flex>
+            <SortButton sort_type=SortType::Title sort_order />
+            <SortButton sort_type=SortType::Score sort_order />
+        </Flex>
         <Suspense fallback=move || {
             view! { <p>"Loading list"</p> }
         }>
-            <TabList selected_value=sort_key>
-                <Tab value="title">Title</Tab>
-                <Tab value="score">Score</Tab>
-            </TabList>
             <div class="search">
                 {move || Suspend::new(async move {
                     let resource = resource.await.expect("Should have movies");
-                    let sort_order = sort_order.await;
-                    let sorted = sort_movies(sort_order, &resource);
-                    sorted
-                        .iter()
-                        .map(|t| resource.get(t).unwrap())
+                    let ids = sort_order.get().sort_movies(&resource);
+                    ids.iter()
+                        .map(|i| resource.get(i).unwrap())
                         .map(
                             &move |movie: &Movie| {
                                 view! { <MovieCard movie=movie.clone() /> }
@@ -50,28 +46,53 @@ pub(crate) fn movie_list() -> impl IntoView {
     }
 }
 
-fn sort_movies(sort_order: SortOrder, movies: &IdHashMap<Movie>) -> Vec<u64> {
-    let mut titles = movies.iter().map(|m| m.base.movie_id).collect::<Vec<_>>();
-    match sort_order {
-        SortOrder::Title => titles.sort_unstable_by_key(|t| &movies.get(t).unwrap().base.title),
-        SortOrder::Score => titles
-            .sort_unstable_by_key(|t| (movies.get(t).unwrap().base.vote_average * 100.) as u32),
-    }
-    titles
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Default, Hash)]
+struct SortOrder {
+    reversed: bool,
+    sort_type: SortType,
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-enum SortOrder {
+impl SortOrder {
+    fn sort_movies(&self, movies: &IdHashMap<Movie>) -> Vec<u64> {
+        let mut titles = movies.iter().map(|m| m.base.movie_id).collect::<Vec<_>>();
+        match self.sort_type {
+            SortType::Title => {
+                let key = |t| &movies.get(&t).unwrap().base.title;
+                titles.sort_unstable_by(|l, r| self.sort_items(*l, *r, key));
+            }
+            SortType::Score => {
+                let key = |t| (movies.get(&t).unwrap().base.vote_average * 100.) as u32;
+                titles.sort_unstable_by(|l, r| self.sort_items(*l, *r, key));
+            }
+        }
+        titles
+    }
+
+    fn sort_items<I: Ord>(&self, l: u64, r: u64, key: impl Fn(u64) -> I) -> Ordering {
+        let l = key(l);
+        let r = key(r);
+        if self.reversed {
+            r.cmp(&l)
+        } else {
+            l.cmp(&r)
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Default, Hash)]
+enum SortType {
+    #[default]
     Title,
     Score,
 }
 
-impl SortOrder {
-    fn from_str(s: &str) -> Self {
-        match s {
-            "title" => Self::Title,
-            "score" => Self::Score,
-            _ => panic!("unknown sort key"),
+use std::fmt::Display as StdDisplay;
+
+impl StdDisplay for SortType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            SortType::Title => write!(f, "Title"),
+            SortType::Score => write!(f, "Score"),
         }
     }
 }
